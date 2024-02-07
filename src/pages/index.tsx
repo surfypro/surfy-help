39219ee2c2029dfa42e226fbf28f97ab35e5d466
vincentdem <vincent@surfy.pro>
2024-02-07 15:28:36 +0100

    accueil

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
+import { Youtube } from '../components/videos/Youtube';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -18,18 +19,27 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Voir la documentation
+            Voir les tutoriels
           </Link>
         </div>
         <div className='hero__subtitle' style={{ marginTop: 16 }}>
+
+        <Youtube code="BRBaZOa-lQo"/> <br />
+
           <p>
-            Nous souhaitons rassembler ici toutes la documentation de l'application Surfy.<br />
-            Ce site est encore en construction.<br />
-            Nous vous invitons à nous faire part de vos remarques et suggestions.
+
+            Bienvenue à la Surfy Academy<br />
+            Vous trouverez ici notre guide utilisateur vous permettant de découvrir Surfy,<br />
+
+            ou d'approfondir vos connaissances sur ses fonctionnalités en choisissant l'onglet "Tutoriels".<br />
+
+            Pour découvrir l'historique des nouveautés choisissez l'onglet "Nouveautés"<br />
+
+            Bon Surf(y) !
           </p>
         </div>
-
       </div>
+      
     </header>
   );
 }
