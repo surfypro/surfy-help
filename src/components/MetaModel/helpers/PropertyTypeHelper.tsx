8ea272e0f8e8671385141b67308a9876f1f2d372
pouya <pouyajoon@gmail.com>
2025-02-10 18:49:04 +0100

    wip playing with app build

--- src/components/MetaModel/helpers/PropertyTypeHelper.tsx
+++ src/components/MetaModel/helpers/PropertyTypeHelper.tsx
@@ -4,15 +4,20 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import ContentCopyTwoToneIcon from "@mui/icons-material/ContentCopyTwoTone";
 import { IconButton } from "@mui/material";
 import { PropertyType } from "../PropertyType";
+import { useTranslation } from '@site/surfy';
 
 export function PropertyTypeHelper(props: { code: string }) {
     const { code } = props;
+
+    const translate = useTranslation('help');
 
     if (!isDevMode()) {
         return null;
     }
     const text = `<P code="${code}" />`;
     return <>
+        {translate('key1', { name: 'Joh2n' })}
+        {translate('key2', { name: 'Joh2n' })}
         <code>
             {text}
         </code>
