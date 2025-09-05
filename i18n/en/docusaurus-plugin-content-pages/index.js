e3dccffe29b77f2d58178127b4bcb372f7d46b53
vincentdem <vincent@surfy.pro>
2025-09-05 18:28:20 +0200

    en

--- i18n/en/docusaurus-plugin-content-pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Redirect } from '@docusaurus/router';
-
-function Home() {
-    return <Redirect to="/en/docs/tutorials/intro" />;
-}
-
-export default Home;
---
+++ i18n/en/docusaurus-plugin-content-pages/index.md
@@ -0,0 +1,11 @@
+---
+slug: /
+---
+
+# Welcome to Surfy Academy
+
+Redirecting to English tutorials...
+
+<script>
+window.location.href = '/en/docs/tutorials/intro';
+</script>
