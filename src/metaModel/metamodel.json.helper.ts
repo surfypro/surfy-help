94251bf49ca5443a456c21a5831d74e1df839ff2
pouya <pouyajoon@gmail.com>
2026-01-08 12:25:08 +0100

    Remove English entity path mapping file and update imports in metamodel helper to reflect this change.

--- src/metaModel/metamodel.json.helper.ts
+++ src/metaModel/metamodel.json.helper.ts
@@ -1,7 +1,5 @@
 import { ObjectTypePathMapping } from "./metamodel.models";
 import entitiesPath from './entities.paths.json';
-import entitiesPathEn from './entities.paths.en.json';
 
 export const objectTypePathMapping: ObjectTypePathMapping = entitiesPath.objectTypePathMapping;
-export const objectTypePathMappingEn: ObjectTypePathMapping = entitiesPathEn.objectTypePathMapping;
 
